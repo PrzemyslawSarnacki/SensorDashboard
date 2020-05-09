28de3544334969dc8cbffe404556075c48c51bd2
PrzemyslawSarnacki <przemos100@gmail.com>
2020-05-09 15:58:53 +0200

    Build include

---
+++ build/precache-manifest.5ab7b628760aa4cf45c76ff0916e3f79.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "506be66f4eef4273cfb4e0f52741411a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8761b0f22474f28f1d33",
+    "url": "/static/css/2.37d5005e.chunk.css"
+  },
+  {
+    "revision": "da24a8280e7f4bfa82fc",
+    "url": "/static/css/main.8514417c.chunk.css"
+  },
+  {
+    "revision": "8761b0f22474f28f1d33",
+    "url": "/static/js/2.1a0f423a.chunk.js"
+  },
+  {
+    "revision": "a8d4c4133e1f1d8f36bee3438457557d",
+    "url": "/static/js/2.1a0f423a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da24a8280e7f4bfa82fc",
+    "url": "/static/js/main.ceb328b8.chunk.js"
+  },
+  {
+    "revision": "7269b0c4207b44d8a9e4f0b85c8a81d2",
+    "url": "/static/js/main.ceb328b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "084c3b7a1b505cca0b17",
+    "url": "/static/js/runtime-main.388b1285.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  }
+]);
