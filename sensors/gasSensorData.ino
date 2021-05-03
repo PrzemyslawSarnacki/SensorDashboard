cf7120ea6754778542725c493e4eefe3685abd65
PrzemyslawSarnacki <przemos100@gmail.com>
2021-05-03 18:04:14 +0200

    library change

--- sensors/gasSensorData.ino
+++ sensors/gasSensorData.ino
@@ -3,62 +3,95 @@
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
 #include "C:/Users/Przemyslaw/Projects/SensorDashboard/sensors/secretFile.h"
+#include <MQUnifiedsensor.h>
 
-const int gasPin = A0; //GAS sensor output pin to Arduino analog A0 pin
+
+//Definicje
+#define placa "Arduino UNO" 
+#define Voltage_Resolution 5  
+#define pin A0 //Wejście analogowe A0
+#define type "MQ-131" //typ czujnika - MQ131
+#define ADC_Bit_Resolution 10 // Rozdzielczość bitowa
+#define RatioMQ131CleanAir 15 //RS / R0 = 15 ppm
+
+//Deklaracja sensora MQ131
+MQUnifiedsensor MQ131(placa, Voltage_Resolution, ADC_Bit_Resolution, pin, type);
 
 void setup()
 {
 
     const char ssid[] = WIFI_SSID;
     const char password[] = WIFI_PASSW;
-    Serial.begin(9600);         //Serial connection
-    WiFi.begin(ssid, password);   //WiFi connection
+    Serial.begin(9600);         //Połączenie szeregowe (predkosc 9600)
+    WiFi.begin(ssid, password);   //Połączenie WiFi z siecią o danym ssid i haśle
     pinMode(LED_BUILTIN, OUTPUT); // Initialize the LED_BUILTIN pin as an output
 
     while (WiFi.status() != WL_CONNECTED)
-    { //Wait for the WiFI connection completion
+    { //Zaczekaj WiFI na zakończenie procesu łączenia z siecią
 
         delay(500);
         Serial.println("Waiting for connection");
     }
+
+  MQ131.setRegressionMethod(1); //_PPM =  a*ratio^b
+  MQ131.init(); 
+  Serial.print("Calibrating please wait.");
+  float calcR0 = 0;
+  for(int i = 1; i<=10; i ++)
+  {
+    MQ131.update(); // Pobierz dane z czujnika
+    calcR0 += MQ131.calibrate(RatioMQ131CleanAir);
+    Serial.print(".");
+  }
+  MQ131.setR0(calcR0/10);
+  Serial.println("  done!.");
+  
+  if(isinf(calcR0)) {Serial.println("Warning: Conection issue founded, R0 is infite (Open circuit detected) please check your wiring and supply"); while(1);}
+  if(calcR0 == 0){Serial.println("Warning: Conection issue founded, R0 is zero (Analog pin with short circuit to ground) please check your wiring and supply"); while(1);}
+
+  MQ131.serialDebug(true);
+  Serial.println("Ignore Ratio = RS/R0, for this example we will use readSensorR0Rs, the ratio calculated will be R0/Rs. Thanks :)");
+
 }
 
 void loop()
 {
 
-    if (WiFi.status() == WL_CONNECTED)
-    {                                       //Check WiFi connection status
-        StaticJsonDocument<200> JSONbuffer; //Declaring static JSON buffer
-        long sensorValue = analogRead(gasPin);
-        // 13 is ID for gas sensor
+    if (WiFi.status() == WL_CONNECTED)//Spradź status połączenia WiFi 
+    {                                       
+        StaticJsonDocument<200> JSONbuffer; //Deklaracja bufora JSON 
+        MQ131.setA(605.18); MQ131.setB(-3.937); // Ustalenie współczynników dla poszczególnego gazu (CO) 
+        float CO = MQ131.readSensor(); // Odczytanie wartosci wg. ustalonych współczynnikow 
+        // 13 to ID dla CO
         JSONbuffer["sensor_type"] = 13;
-        JSONbuffer["value"] = sensorValue;
+        JSONbuffer["value"] = CO;
         char JSONmessageBuffer[300];
         serializeJsonPretty(JSONbuffer, JSONmessageBuffer, sizeof(JSONmessageBuffer));
         Serial.println(JSONmessageBuffer);
 
-        HTTPClient http; //Declare object of class HTTPClient
+        HTTPClient http; //Declararacja obiektu klasy HTTPClient
 
-        http.begin("http://sensor-dashboards.herokuapp.com/api/add-data/"); //Specify request destination
-        http.addHeader("Content-Type", "application/json");   //Specify content-type header
+        http.begin("http://sensor-dashboards.herokuapp.com/api/add-data/"); //Adres pod, którym wykonywane bedzie żądanie
+        http.addHeader("Content-Type", "application/json");   //Opis naglowka
 
-        int httpCode = http.POST(JSONmessageBuffer); //Send the request
-        String payload = http.getString(); //Get the response payload
+        int httpCode = http.POST(JSONmessageBuffer); //Wyslanie żądania 
+        String payload = http.getString(); //Sprawdzenie payloadu 
         digitalWrite(LED_BUILTIN, LOW);
         delay(1000);
         digitalWrite(LED_BUILTIN, HIGH);
 
-        Serial.println("httpCode:"); //Print HTTP return code
-        Serial.println(httpCode); //Print HTTP return code
-        Serial.println("Payload:");  //Print request response payload
-        Serial.println(payload);  //Print request response payload
+        Serial.println("httpCode:"); 
+        Serial.println(httpCode); //Wyswietl kod HTTP (Jeśli, będzie to 200 to znaczy, że operacja powiodła się)
+        Serial.println("Payload:"); 
+        Serial.println(payload);  //Payload
 
-        http.end();  //Close connection
-        delay(3000); // Wait for two seconds (to demonstrate the active low LED)
+        http.end();  //Zamknij połączenie
+        delay(3000); 
     }
     else
     {
         Serial.println("Error in WiFi connection");
     }
-    delay(24*3600*1000);
+    Serial.println("Deep sleep"); 
+    ESP.deepSleep(24*60*60e6); //Uśpij na 24h
 }
